b9ae248750af1dddac286f74acee1d21db5925d4
limhaekyu <limk9500@gmail.com>
2023-05-05 15:31:46 +0900

    Initial commit

---
+++ src/main/java/com/board_mybatis_thymeleaf/BoardMybatisThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.board_mybatis_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardMybatisThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardMybatisThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/board_mybatis_thymeleaf/BoardMybatisThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board_mybatis_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardMybatisThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
